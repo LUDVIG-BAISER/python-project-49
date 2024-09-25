49d9e8a559e5feb8bb275c3e8d6a8be1eecc1aa4
Evgenii Akulinin <Evgen.ak24@gmail.com>
2024-09-25 19:14:02 +0400

    refactoring project

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,11 +1,7 @@
 import random
 
-
 def is_even(number):
     return number % 2 == 0
-
-def even_game():
-    print('Answer "yes" if the number is even, otherwise answer "no".')
 
 def generate_question_and_answer():
     number = random.randint(1, 100)
@@ -13,4 +9,3 @@ def generate_question_and_answer():
     correct_answer = "yes" if is_even(number) else "no"
 
     return question, correct_answer
-
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -2,6 +2,7 @@ from brain_games.game_engine.base import play_game
 from brain_games.games import even
 
 def main():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
     play_game(even)
 
 if __name__ == "__main__":
