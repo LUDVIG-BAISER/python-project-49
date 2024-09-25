0c06ff330649f270fe3da7a9770d49ff1085f954
Evgenii Akulinin <Evgen.ak24@gmail.com>
2024-09-26 00:27:55 +0400

    refactoring project

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -2,7 +2,7 @@ from brain_games.game_engine.base import play_game
 from brain_games.games import prime
 
 def main():
-    print('"yes" if given number is prime. Otherwise answer "no".')
+    print('Answer "yes" if given number is prime. Otherwise answer "no".')
     play_game(prime)
 
 if __name__ == "__main__":
