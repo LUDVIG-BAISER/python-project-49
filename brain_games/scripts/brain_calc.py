85ebc73892a3ed64425ad82025e138fffb60ff08
Evgenii Akulinin <Evgen.ak24@gmail.com>
2024-09-25 19:41:30 +0400

    refactoring project

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -4,6 +4,7 @@ from brain_games.game_engine.base import play_game
 from brain_games.games import calc
 
 def main():
+    print('What is the result of the expression?')
     play_game(calc)
 
 if __name__ == "__main__":
