86448f440dcc7dcd9fa9e83c90d44feff1d6ef02
Evgenii Akulinin <Evgen.ak24@gmail.com>
2024-09-26 00:06:28 +0400

    refactoring project

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -2,6 +2,7 @@ from brain_games.game_engine.base import play_game
 from brain_games.games import gcd
 
 def main():
+    print('Find the greatest common divisor of given numbers')
     play_game(gcd)
 
 if __name__ == "__main__":
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -2,6 +2,7 @@ from brain_games.game_engine.base import play_game
 from brain_games.games import progression
 
 def main():
+    print('What number is missing in the progression?')
     play_game(progression)
 
 if __name__ == "__main__":
