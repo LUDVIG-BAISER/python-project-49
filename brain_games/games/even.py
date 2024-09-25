e037774506bd69e0b026fd8df378d236b628f570
Evgenii Akulinin <Evgen.ak24@gmail.com>
2024-09-25 17:13:51 +0400

    refactoring project

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -4,6 +4,8 @@ import random
 def is_even(number):
     return number % 2 == 0
 
+def even_game():
+    print('Answer "yes" if the number is even, otherwise answer "no".')
 
 def generate_question_and_answer():
     number = random.randint(1, 100)
