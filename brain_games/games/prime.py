061dcd2a5a395b5b3331884d05ecd1e02f2652ba
Evgenii Akulinin <evgen.ak24@gmail.com>
2024-10-03 16:15:17 +0500

    refactoring project

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,11 +1,11 @@
 import random
-
+import math
 
 def is_prime(number):
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
     if number < 2:
         return False
-    for i in range(2, number):
+    for i in range(2, int(math.sqrt(number)) + 1):
         if number % i == 0:
             return False
     return True
--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -9,13 +9,9 @@ def generate_question_and_answer():
     progression = [start + i * step for i in range(length)]
 
     hidden_index = random.randint(0, length - 1)
-    correct_answer = progression[hidden_index]
 
-    progression[hidden_index] = ".."
+    progression[hidden_index], correct_answer = "..", progression[hidden_index]
 
     question = " ".join(map(str, progression))
 
     return question, str(correct_answer)
-
-
-
