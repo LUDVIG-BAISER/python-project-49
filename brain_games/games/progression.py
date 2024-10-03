fe49da642b58c00495868813310495e1a0eb567b
Evgenii Akulinin <evgen.ak24@gmail.com>
2024-10-03 17:26:40 +0500

    refactoring project

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -4,13 +4,13 @@ import random
 def generate_question_and_answer() -> tuple[str, str]:
     print('What number is missing in the progression?')
     start: int = random.randint(1, 10)
-    step:int = random.randint(1, 5)
+    step: int = random.randint(1, 5)
     length: int = 10
     progression: list[int] = [start + i * step for i in range(length)]
 
     hidden_index: int = random.randint(0, length - 1)
 
-    progression[hidden_index], correct_answer  = "..", progression[hidden_index]
+    progression[hidden_index], correct_answer = "..", progression[hidden_index]
 
     question: str = " ".join(map(str, progression))
 
